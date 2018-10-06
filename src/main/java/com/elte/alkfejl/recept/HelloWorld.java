8202a27960048eeda12e4b1fd545b14ccfb5dc46
robence <robence@gmail.com>
2018-10-06 21:11:17 +0200

    init repo

---
+++ src/main/java/com/elte/alkfejl/recept/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.elte.alkfejl.recept;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorld {
+
+    @GetMapping("/")
+    public String helloWorld() {
+        return "Hello World!";
+    }
+}
---
+++ src/main/java/com/elte/alkfejl/recept/ReceptApplication.java
@@ -0,0 +1,12 @@
+package com.elte.alkfejl.recept;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReceptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReceptApplication.class, args);
+	}
+}
